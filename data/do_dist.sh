366f69ef2e4acd79be46ea9cc278bfa67b7f784c
seva@3088d786-d643-db11-b331-00a0c9b636be <seva@3088d786-d643-db11-b331-00a0c9b636be>
2008-05-08 05:28:25 +0000

    разбиралка картриджа и отдельный архив про железку
    --HG--
    extra : convert_revision : svn%3A3088d786-d643-db11-b331-00a0c9b636be/sandbox/seva/robopanda%408244

--- data/do_dist.sh
+++ data/do_dist.sh
@@ -4,8 +4,10 @@ CDIR=`pwd`
 TDIR=`mktemp -d /var/tmp/zzXXXX`
 cd $CDIR
 svn export svn+ssh://rocky/data/ipnet/svn/sandbox/seva/robopanda robopanda_tools
-rm -r robopanda_tools/virtex
+mv robopanda_tools/virtex robopanda_hwemu
 
-tar cvzf $CDIR/robopanda_tools-`date +%Y%m%d%H%M`.tar.gz robopanda_tools
+VER=`date +%Y%m%d%H%M`
+tar cvzf $CDIR/robopanda_tools-${VER}.tar.gz robopanda_tools
+tar cvzf $CDIR/robopanda_hwemu-${VER}.tar.gz robopanda_hwemu
 rm -r $TDIR
 
