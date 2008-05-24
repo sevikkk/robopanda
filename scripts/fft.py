e929117746474abab6a495977231eba61706de31
seva@3088d786-d643-db11-b331-00a0c9b636be <seva@3088d786-d643-db11-b331-00a0c9b636be>
2008-05-24 22:44:09 +0000

    автоподстройка частоты и коррекция фазы из-за фильтра на выходе
    --HG--
    extra : convert_revision : svn%3A3088d786-d643-db11-b331-00a0c9b636be/sandbox/seva/robopanda%408376

--- scripts/fft.py
+++ scripts/fft.py
@@ -56,6 +56,7 @@ def tune(fn, start, end, shift = 0, corr = 0):
             break
 
         data = struct.unpack("h"*frame_len, data)
+        data = [ -a for a in data ]
 
         fd = FFT.real_fft(data, frame_len).tolist()
         f = 62.5
@@ -64,7 +65,7 @@ def tune(fn, start, end, shift = 0, corr = 0):
         phase_500 = None
         for a in fd[1:]:
             p = abs(a)
-            g = math.atan2(a.real,a.imag)/math.pi*180
+            g = math.atan2(a.imag,a.real)/math.pi*180
             if p>100000:
                 print "%8.3f: %7d %4d"% (f,p,g),
             if f == 500:
@@ -75,7 +76,7 @@ def tune(fn, start, end, shift = 0, corr = 0):
                 break
         print
         if power_500>1000000:
-            shift = (-90 - phase_500)/360*96
+            shift = phase_500/360*96
             print "shift", shift
             shifts.append(shift)
         else:
@@ -120,8 +121,17 @@ def analyze(fn, corr = 0.8, shift = 4, frames = None, resync = None):
     
     frame_num = 0
 
+    phase_corr = {
+            562.5:  -3,
+            750.0:  -20,
+            812.5:  -25,
+            875.0:  -30,
+            937.5:  -35,
+            1000.0: -40,
+    }
+
     while 1:
-        if corr_sum > 1:
+        if corr_sum >= 1:
             ns = int(corr_sum)
             corr_sum -= ns
             rdata.readframes(ns)
@@ -130,7 +140,7 @@ def analyze(fn, corr = 0.8, shift = 4, frames = None, resync = None):
         elif corr_sum < -1:
             ns = int(-corr_sum)
             corr_sum += ns
-            data = data[-ns:] + rdata.readframes(frame_len - ns)
+            data = struct.pack("h"*ns, *data[-ns:]) + rdata.readframes(frame_len - ns)
             #print "shifted %d samples backward" % ns
         else:
             data = rdata.readframes(frame_len)
@@ -142,52 +152,73 @@ def analyze(fn, corr = 0.8, shift = 4, frames = None, resync = None):
             break
 
         data = struct.unpack("h"*frame_len, data)
+        data = [ -a for a in data ]
 
         fd = FFT.real_fft(data, frame_len).tolist()
         f = 62.5
+        do_print = 0
+
         if (not frames) or (frame_num in frames):
+            do_print = 1
             print "%6d: "% (frame_num,),
-            power_500 = 0
-            phase_500 = None
-            for a in fd[1:]:
-                p = abs(a)
-                g = math.atan2(a.real,a.imag)/math.pi*180
-                if p>100000:
-                    print "%8.3f: %7d %4d"% (f,p,g),
 
-                if f == 500:
-                    power_500 = p
-                    phase_500 = g
+        power_500 = 0
+        phase_500 = None
+        for a in fd[1:]:
+            p = abs(a)
+            g = math.atan2(a.imag,a.real)/math.pi*180
+            pc = phase_corr.get(f,0)
+            if do_print and (p>100000):
+                print "%8.3f: %7d %4d"% (f,p,g + pc),
 
-                f += 62.5
-                if f >= 4000:
-                    break
+            if f == 500:
+                power_500 = p
+                phase_500 = g
+
+            f += 62.5
+            if f >= 4000:
+                break
+        if do_print:
             print
-
-        corr_sum += corr
 
         if resync and frame_num in resync:
             if power_500>1000000:
-                ns = (-90 - phase_500)/360*96
-                print "need_resync", -ns, "instead of", corr_sum, "error", (corr_sum + ns)/(frame_num - last_resync)
+                ns = phase_500/360*96
+                #print "need_resync", -ns, "instead of", corr_sum, "error", (corr_sum + ns)/(frame_num - last_resync)
+                corr -= (corr_sum + ns)/(frame_num - last_resync)
+
                 last_resync = frame_num
                 corr_sum = -ns
+
+        corr_sum += corr
 
         frame_num += 1
 
 
 if __name__ == "__main__":
-    shift = 677
-    corr = 0.931128818452
+    shift = 177
+    corr = 0.926335885172
     if 0:
-        tune(sys.argv[1], 18, 41, shift, corr)
-    if 1:
-        analyze(sys.argv[1], shift = shift, corr = corr, 
-                resync = [16 + n*31 for n in range(200)])
+        # find corr and initial value of shift
+        tune(sys.argv[1], 13, 20, shift, corr)
     if 0:
+        # find corrected value of shift for 562.5Hz align
         for shift2 in range(10):
             print "shift", shift + shift2*96
-            analyze(sys.argv[1], shift = shift+shift2*96, corr = corr, frames = [27,49,60,70,82])
+            analyze(sys.argv[1], shift = shift+shift2*96, corr = corr, frames = range(14,21))
+    if 0:
+        # fing final value of shift
+        for shift2 in range(-10, 10):
+            print "shift", shift + shift2
+            analyze(sys.argv[1], shift = shift+shift2, corr = corr, frames = range(14,21))
+    if 0:
+        #full decode for determining of resync and data frames
+        analyze(sys.argv[1], shift = shift, corr = corr)
+    if 1:
+        #decode only needed frames
+        analyze(sys.argv[1], shift = shift, corr = corr, 
+                resync = [13 + n*31 for n in range(200)],
+                frames = [25 + n*31 for n in range(200)])
 
     
 
