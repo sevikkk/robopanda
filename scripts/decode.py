1d51304368cd35189ac9a7a14e9116e8b9082c5a
seva@3088d786-d643-db11-b331-00a0c9b636be <seva@3088d786-d643-db11-b331-00a0c9b636be>
2008-06-27 19:01:54 +0000

    почти совсем живое
    --HG--
    extra : convert_revision : svn%3A3088d786-d643-db11-b331-00a0c9b636be/sandbox/seva/robopanda%408514

--- scripts/decode.py
+++ scripts/decode.py
@@ -15,25 +15,6 @@ class Meta:
     def __init__(self, frame_len):
         self.frame_len = frame_len
 
-        self.freq_resp = {
-            0:	5000,
-            1:	4920,
-            2:	4715,
-            3:	4469,
-            4:	4141,
-            5:	3785,
-            6:	3416,
-            7:	3047,
-            8:	2590,
-            9:	2360,
-            10:	2049,
-            11:	1765,
-            12:	1503,
-            13:	1267,
-            14:	1057,
-            15:	871
-        }
-
         self.main_volumes = []
         k = 1
         for i in range(15):
@@ -43,7 +24,7 @@ class Meta:
     
         self.local_volumes = [1.0]
         k = 0.75
-        for i in range(7):
+        for i in range(6):
             self.local_volumes.append(k)
             if i<4:
                 k = k * 0.75
@@ -54,12 +35,64 @@ class Meta:
         self.filter1 = array([0.0] * frame_len)
         self.filter2 = array([0.0] * frame_len)
         for i in range(frame_len):
-            w = math.sin(math.pi/2*i/frame_len)
-            w=w*w
+            #w = math.sin(math.pi/2*i/frame_len)
+            #w=w*w
+            w = 1.0*i/frame_len
             self.filter1[i] = w
             self.filter2[frame_len-1 - i] = w
 
+    def get_main_volume(self, val):
+        volume = 15
+        for k in self.main_volumes[1:]:
+            if val>k:
+                break
+            volume -= 1
+
+        return volume
+
+    def get_local_volume(self, main_volume, val):
+        main_volume_k = self.main_volumes[15-main_volume]
+        sub_volume = 7
+        for k in self.local_volumes[1:]:
+            if val > k * main_volume_k:
+                break
+            else:
+                sub_volume -= 1
+
+        return sub_volume
+
 meta = Meta(512)
+
+class Filter:
+    def __init__(self):
+        self.history = [0.0] * (meta.frame_len/64)
+        self.max = 0
+        self.clips = 0
+
+    def next(self, val):
+        self.history = self.history[1:] + [val]
+        val = sum(self.history)/len(self.history)
+        val = int(val)
+
+        if -val>self.max:
+            self.max = -val
+
+        if val>self.max:
+            self.max = val
+
+        if val>16000:
+            print "WARNING: clipping %d to 16000" % self.max
+            val = 16000
+            self.clips += 1
+
+        if val<-16000:
+            print "WARNING: clipping -%d to -16000" % self.max
+            val = -16000
+            self.clips += 1
+
+        return val
+
+gencache = {}
 
 class SubBand:
     def __init__(self, freq, phase):
@@ -74,8 +107,9 @@ class SubBand:
                 self.freq, self.phase, self.volume,  self.inverse)
 
     def generate(self, frame):
-        fi = self.phase*math.pi/180
-        delta_fi = self.freq*2*math.pi*0.016/len(frame)
+
+        if self.volume == 0:
+            return
 
         k = meta.local_volumes[8 - self.volume]
         if self.inverse:
@@ -83,10 +117,22 @@ class SubBand:
 
         k = -k # robopanda output is inverted
 
-        for i in range(len(frame)):
-            x = math.sin(fi) * k
-            frame[i] += x
-            fi += delta_fi
+        cache_key = (self.phase, self.freq, k)
+
+        if cache_key in gencache:
+            frame_data = gencache[cache_key]
+        else:
+            fi = self.phase*math.pi/180
+            delta_fi = self.freq*2*math.pi*0.016/len(frame)
+
+            frame_data = array([0.0] * meta.frame_len)
+            for i in range(len(frame)):
+                x = math.sin(fi) * k
+                frame_data[i] = x
+                fi += delta_fi
+            gencache[cache_key] = frame_data
+
+        frame += frame_data
 
 class Band:
     def __init__(self, band):
@@ -103,7 +149,6 @@ class Band:
                 (3,   0), #5    687.5
                 (3, -90), #6    687.5
                 (4,  90), #7    750.0
-                (4,   0), #8    750.0 (only in 2 nibbles format)
             ]:
             self.subbands.append(SubBand(self.base_freq + 62.5 * freq, phase))
 
@@ -124,25 +169,78 @@ class Band:
                 (self.band, self.base_freq, self.volume, self.main_subband, 
                     self.inverse, self.format, self.frame_offset)]
 
-        for i in range(9):
-            if self.subbands[i].volume > 0:
-                s.append("\t"+str(self.subbands[i])+"\n")
+        for i in range(8):
+            #if self.subbands[i].volume > 0:
+            #    s.append("\t"+str(self.subbands[i])+"\n")
+            s.append("\t"+str(self.subbands[i])+"\n")
 
         s.append("]")
 
         return "".join(s)
 
     def encode(self, frame):
+        #main volume
         frame[self.band] = self.volume
-        for i in range(self.num_subs):
-            frame[self.frame_offset+i] = self.subbands[i].encode()
+
+        ms = self.subbands[self.main_subband]
+        assert ms.volume == 8
+
+        subframe = [0] * 8
+
+        #main inverse
+        if self.format in (6,8):
+            self.inverse = self.subbands[0].inverse
+            for b in range(1,8):
+                pb = self.subbands[b-1]
+                cb = self.subbands[b]
+                if b <= self.main_subband:
+                    subframe[b] = pb.volume | (cb.inverse << 3)
+                else:
+                    subframe[b] = (cb.volume<<1) | cb.inverse
+
+            if self.format == 6:
+                for b in range(4,8):
+                    if b <= self.main_subband:
+                        subframe[b] = subframe[b]>>2 # ia00 -> ia
+                    else:
+                        subframe[b] = ((subframe[b] & 8) >> 2) | (subframe[b] & 1) # a00i -> ai
+                subframe[4] = (subframe[5] << 2) | subframe[4]
+                subframe[5] = (subframe[7] << 2) | subframe[6]
+
+        elif self.format == 2:
+            if self.main_subband == 0:
+                self.inverse = ms.inverse
+                b0 = self.subbands[1].inverse
+                b1 = self.subbands[1].volume >> 2
+                b2 = self.subbands[2].inverse
+                b3 = self.subbands[2].volume >> 2
+            elif self.main_subband == 7:
+                self.inverse = self.subbands[5].inverse
+                b0 = self.subbands[5].volume >> 2
+                b1 = self.subbands[6].inverse
+                b2 = self.subbands[6].volume >> 2
+                b3 = self.subbands[7].inverse
+            else:
+                self.inverse = self.subbands[self.main_subband-1].inverse
+                b0 = self.subbands[self.main_subband-1].volume >> 2
+                b1 = self.subbands[self.main_subband].inverse
+                b2 = self.subbands[self.main_subband+1].inverse
+                b3 = self.subbands[self.main_subband+1].volume >> 2
+            subframe[1] = b0 | (b1 << 1) | (b2 << 2) | (b3 << 3)
+        else:
+            self.inverse = ms.inverse 
+
+        subframe[0] = (self.inverse << 3) | self.main_subband
+
+        for i in range(self.format):
+            frame[self.frame_offset + i] = subframe[i]
 
     def decode(self, frame):
         self.volume = frame[self.band]
         self.main_subband = frame[self.frame_offset] & 0x7
         self.inverse = frame[self.frame_offset] >> 3
 
-        for b in range(9):
+        for b in range(8):
             cs = self.subbands[b]
             cs.volume = 0
             cs.inverse = 0
@@ -164,7 +262,7 @@ class Band:
                     if b <= self.main_subband:
                         subframe[b] = subframe[b]<<2 # ia -> ia00
                     else:
-                        subframe[b] = ((subframe[b] & 2) << 3) | (subframe[b] & 1) # ai -> a00i
+                        subframe[b] = ((subframe[b] & 2) << 2) | (subframe[b] & 1) # ai -> a00i
 
             offset = 1
             for b in range(8):
@@ -181,21 +279,37 @@ class Band:
                     offset += 1
 
         elif self.format == 2:
-            sub1_volume = (frame[self.frame_offset+1] & 1) << 3
-            sub1_inverse = (frame[self.frame_offset+1] & 2) >> 1
-            sub2_volume = (frame[self.frame_offset+1] & 4) >> 1
-            sub2_inverse = (frame[self.frame_offset+1] & 8) >> 3
-
-            self.subbands[self.main_subband-1].volume = sub1_volume
-            self.subbands[self.main_subband-1].inverse = sub1_inverse
-            self.subbands[self.main_subband+1].volume = sub2_volume
-            self.subbands[self.main_subband+1].inverse = sub2_inverse
+            dat = frame[self.frame_offset+1]
+            b3 = (dat & 8)>>3
+            b2 = (dat & 4)>>2
+            b1 = (dat & 2)>>1
+            b0 = dat & 1
+            if self.main_subband == 0:
+                self.subbands[0].volume = 8
+                self.subbands[0].inverse = self.inverse
+                self.subbands[1].volume = b1*4
+                self.subbands[1].inverse = b0
+                self.subbands[2].volume = b3*4
+                self.subbands[2].inverse = b2
+            elif self.main_subband == 7:
+                self.subbands[7].volume = 8
+                self.subbands[7].inverse = b3
+                self.subbands[5].volume = b0*4
+                self.subbands[5].inverse = self.inverse
+                self.subbands[6].volume = b2*4
+                self.subbands[6].inverse = b1
+            else:
+                self.subbands[self.main_subband].inverse = b1
+                self.subbands[self.main_subband-1].volume = b0*4
+                self.subbands[self.main_subband-1].inverse = self.inverse
+                self.subbands[self.main_subband+1].volume = b3*4
+                self.subbands[self.main_subband+1].inverse = b2
 
     def generate(self, frame):
         band_frame = array([0.0] * len(frame))
-        for i in range(9):
+        for i in range(8):
             self.subbands[i].generate(band_frame)
-        frame += band_frame * array([meta.main_volumes[15-self.volume]*meta.freq_resp[self.band]/5000.0])
+        frame += band_frame * array([meta.main_volumes[15-self.volume]])
 
 class Frame:
     def __init__(self):
@@ -218,29 +332,42 @@ class Frame:
                 Band(15),
         ] 
 
-    def decode(self, frame):
+    def set_format_and_offsets(self, frame):
         volumes = []
         for i in range(16):
-            volumes.append([-frame[i],i])
+            volumes.append([-self.bands[i].volume, i])
             self.bands[i].format = 1
 
         volumes.sort()
         sizes = [8,8,8,6,6,2] 
-        print volumes
+        #print volumes
 
         for v,b in volumes[:6]:
             self.bands[b].format = sizes[0]
             sizes = sizes[1:]
+
         frame_offset = 16
         for b in range(16):
             self.bands[b].frame_offset = frame_offset
             frame_offset += self.bands[b].format
 
+    def decode(self, frame):
+        for i in range(16):
+            self.bands[i].volume = frame[i]
+
+        self.set_format_and_offsets(frame)
+
         for i in range(16):
             self.bands[i].decode(frame)
 
     def encode(self):
+        # assume that for each subband main_subband, volume set correctly
+        # and subband volumes scaled accordingly
+        #
+        # inverse and format will be recalculated
         frame = [0]*64
+
+        self.set_format_and_offsets(frame)
         for i in range(16):
             self.bands[i].encode(frame)
 
@@ -258,7 +385,6 @@ class Frame:
         frame = array([0.0] * frame_len)
         for band in self.bands:
             band.generate(frame)
-        frame *= array([5000.0*2.1])
         return frame
 
     def unpack(self, data):
@@ -292,11 +418,11 @@ def decode(fn, ofn, start, num):
     out.setframerate(int(meta.frame_len*62.5))
     out.setcomptype("NONE","not compressed")
 
-    print "main_volumes_table", meta.main_volumes
-    print "local_volumes_table", meta.local_volumes
+    #print "main_volumes_table", meta.main_volumes
+    #print "local_volumes_table", meta.local_volumes
 
     hdr = input.read(2)
-    print "hdr", `hdr`
+    #print "hdr", `hdr`
 
     if start:
         input.read(32*start)
@@ -304,11 +430,13 @@ def decode(fn, ofn, start, num):
     frame_num = start
 
     prev_result = array([0.0] * meta.frame_len)
+    prev_frame = Frame()
+    filter = Filter()
 
     while 1:
         data = input.read(32)
         if len(data) != 32:
-            print "foot", `data`
+            #print "foot", `data`
             break
 
         frame = Frame()
@@ -316,16 +444,25 @@ def decode(fn, ofn, start, num):
         frame.decode(fdata)
         if 0:
             edata = frame.encode()
-            assert fdata == edata
+            if fdata != edata:
+                print "fdata", fdata
+                print "edata", edata
+                ef = Frame()
+                ef.decode(edata)
+                for b in range(16):
+                    print b
+                    print "original", frame.bands[b]
+                    print "encoded ", ef.bands[b]
+                assert fdata == edata
             edata2 = frame.pack(edata)
             assert data == edata2
 
-        print frame_num, frame
+        #print frame_num, frame
         result = frame.generate().tolist()
-        print max(result), min(result)
+        #print frame_num, max(result), min(result)
 
         out_frame = result * meta.filter1 + prev_result * meta.filter2
-        out_frame = [ int(a*0.5) for a in out_frame ]
+        out_frame = [ int(filter.next(a*4000.0)) for a in out_frame ]
         odata = struct.pack('h'*meta.frame_len,*out_frame)
         out.writeframes(odata)
         prev_result = result
@@ -336,6 +473,7 @@ def decode(fn, ofn, start, num):
             if num == 0:
                 break
 
+    print "max value", filter.max, "clips", filter.clips
     out.close()
 
 if __name__ == "__main__":
--- scripts/encode.py
+++ scripts/encode.py
@@ -9,6 +9,7 @@ import struct
 import sys
 import wave
 import math
+import decode
 
 def encode(fn,ofn):
 
@@ -23,116 +24,15 @@ def encode(fn,ofn):
 
     rate = rdata.getframerate()
     frame_len = int(rate * 0.016)
-    print "using %d samples per frame" % frame_len
+
+    decode.meta = decode.Meta(frame_len)
+
+    #print "using %d samples per frame" % frame_len
 
     out = open(ofn, "wb")
     out.write("\x07\x80")
     frame_num = 0
-    freq_resp = {
-            0:	5000,
-            1:	4920,
-            2:	4715,
-            3:	4469,
-            4:	4141,
-            5:	3785,
-            6:	3416,
-            7:	3047,
-            8:	2590,
-            9:	2360,
-            10:	2049,
-            11:	1765,
-            12:	1503,
-            13:	1267,
-            14:	1057,
-            15:	871
-    }
-    for i in range(16):
-        freq_resp[i] = 5000.0/freq_resp[i]
 
-    freq_map = {
-             62.5:     62.5,
-            125.0:    125.0,
-            187.5:    187.5,
-            250.0:    250.0,
-            312.5:    312.5,
-            375.0:    375.0,
-            437.5:    437.5,
-            500.0:    500.0,
-            562.5:    562.5,
-            625.0:    625.0,
-            687.5:    687.5,
-            750.0:    750.0,
-            812.5:    812.5,
-            875.0:    875.0,
-            937.5:    937.5,
-           1000.0:   1000.0,
-           1062.5:   1062.5,
-           1125.0:   1125.0,
-           1187.5:   1187.5,
-           1250.0:   1250.0,
-           1312.5:   1250.0,
-           1375.0:   1437.5,
-           1437.5:   1437.5,
-           1500.0:   1500.0,
-           1562.5:   1500.0,
-           1625.0:   1500.0,
-           1687.5:   1750.0,
-           1750.0:   1750.0,
-           1812.5:   1750.0,
-           1875.0:   2000.0,
-           1937.5:   2000.0,
-           2000.0:   2000.0,
-           2062.5:   2000.0,
-           2125.0:   2250.0,
-           2187.5:   2250.0,
-           2250.0:   2250.0,
-           2312.5:   2250.0,
-           2375.0:   2500.0,
-           2437.5:   2500.0,
-           2500.0:   2500.0,
-           2562.5:   2500.0,
-           2625.0:   2750.0,
-           2687.5:   2750.0,
-           2750.0:   2750.0,
-           2812.5:   2750.0,
-           2875.0:   3000.0,
-           2937.5:   3000.0,
-           3000.0:   3000.0,
-           3062.5:   3000.0,
-           3125.0:   3250.0,
-           3187.5:   3250.0,
-           3250.0:   3250.0,
-           3312.5:   3250.0,
-           3375.0:   3500.0,
-           3437.5:   3500.0,
-           3500.0:   3500.0,
-           3562.5:   3500.0,
-           3625.0:   3750.0,
-           3687.5:   3750.0,
-           3750.0:   3750.0,
-           3812.5:   3750.0,
-           3875.0:   4000.0,
-           3937.5:   4000.0,
-           4000.0:   4000.0,
-    }
-
-    main_volumes = []
-    k = 1
-    for i in range(15):
-        k = k * 0.6
-        main_volumes.append(k)
-        
-    local_volumes = []
-    k = 0.75
-    for i in range(7):
-        local_volumes.append(k)
-        if i<4:
-            k = k * 0.75
-        else:
-            k = k * 0.6
-
-    print "main_volumes_table", main_volumes
-    print "local_volumes_table", local_volumes
     window = []
     for i in range(frame_len):
         x = math.pi*(i-frame_len/2)/frame_len*2
@@ -141,10 +41,19 @@ def encode(fn,ofn):
         else:
             win = math.sin(x)/x
         window.append(win)
-    print "window", window
+    #print "window", window
 
-    data = rdata.readframes(frame_len)
-    prev_data = struct.unpack("h"*frame_len, data)
+    freq_map = {}
+    for b in range(16):
+        for sb in range(5):
+            f = b * 250.0 + 62.5 * sb
+            if sb == 0:
+                freq_map[f] =  ((b-1,7),(b,0))
+            elif sb == 4:
+                freq_map[f] =  ((b,sb*2-1),(b+1,0))
+            else:
+                freq_map[f] =  ((b,sb*2-1),(b,sb*2))
+
     super_max = 0
     while 1:
         cur_data = rdata.readframes(frame_len)
@@ -154,231 +63,71 @@ def encode(fn,ofn):
 
         cur_data = struct.unpack("h"*frame_len, cur_data)
 
-        prev_data = cur_data
+        frame = decode.Frame()
+
         data = list(cur_data)
 
-        for i in range(frame_len):
-            data[i] = data[i] * window[i]
-
+        #for i in range(frame_len):
+        #    data[i] = data[i] * window[i]
 
         fd = rfft(data, frame_len).tolist()
         f = 62.5
 
-        freqs = []
-        freqs_data = {}
-        freqs_cnt = {}
         for a in fd[1:]:
-            p = abs(a)
-            g = math.atan2(a.imag,a.real)/math.pi*180
 
-            freqs.append([f,p,g,a])
-            mfreq = freq_map[f]
+            (bc, sc), (bs, ss) = freq_map[f]
 
-            if not mfreq in freqs_data:
-                freqs_data[mfreq] = 0
-                freqs_cnt[mfreq] = 0
+            if bc>=0 and bc<=15:
+                frame.bands[bc].subbands[sc].volume = abs(a.real)/500000.0
+                if a.real<0:
+                    frame.bands[bc].subbands[sc].inverse = 1
+                else:
+                    frame.bands[bc].subbands[sc].inverse = 0
 
-            freqs_data[mfreq] += abs(a)
-            freqs_cnt[mfreq] += 1
+            if bs>=0 and bs<=15:
+                frame.bands[bs].subbands[ss].volume = abs(a.imag)/500000.0
+                if a.imag<0:
+                    frame.bands[bs].subbands[ss].inverse = 1
+                else:
+                    frame.bands[bs].subbands[ss].inverse = 0
 
-            if f >= 4000:
+            if f > 4000:
                 break
 
             f += 62.5
 
-        for f in freqs_data.keys():
-            freqs_data[f] = freqs_data[f]/freqs_cnt[f]
+        #print frame
+        for b in range(16):
+            volumes = []
+            max_volume = 0
+            main_subband = 0
+            for sb in range(8):
+                sbo = frame.bands[b].subbands[sb]
+                if sbo.volume>super_max:
+                    super_max = sbo.volume
+                if sbo.volume>max_volume:
+                    max_volume = sbo.volume
+                    main_subband = sb
 
-        if 1:
-            freqs.sort(lambda x,y: cmp(y[1],x[1]))
-            max_val = freqs[0][1]
-            super_max = max(super_max, max_val)
-
-            if max_val>100000:
-                limit = max(10000, max_val / 100)
-            else:
-                limit = 100000000
-
-            print "%6d (%d): "% (frame_num, max_val),
-            for f,p,g,a in freqs:
-                if p>limit:
-                    print "%8.3f: %7d %4d"% (f,p,g),
-
-            print
-
-        k0 = 5000000
-
-        frame = [0] * 64
-
-        for band in (15,14,13,12,11,10,9,8,7,6):
-            sb = []
-
-            val = freqs_data.get(band*250,0)
-            val = abs(val)/k0 * freq_resp[band]
-            sb.append(val)
-
-            volume = 15
-            for k in main_volumes:
-                if val>k:
-                    break
-                volume -= 1
-
-            print "band", band, "val", val, "main volume", volume
-
-            frame[band] = volume
-            offset = 54 + (band-6)
-            frame[offset] = 0 # ignore overlapping subbands
-
-        for band in (5,):
-            sb = []
-            for sband in (0,3): # 1250 and 1437.5
-                val = freqs_data.get(band*250 + sband*62.5,0)
-                val = abs(val)/k0 * freq_resp[band]
-                sb.append(val)
-
-            if sb[0] < sb[1]: # subband value more than main tone
-                if 0:
-                    print "WARN: band", band, "main tone volume less than subband, remix"
-                    sum = (sb[0] + sb[1])/2/1.75
-                    sb[0] = sum
-                    sb[1] = sum * 0.75
+            main_volume = decode.meta.get_main_volume(max_volume)
+            frame.bands[b].volume = main_volume
+            frame.bands[b].main_subband = main_subband
+            for sb in range(8):
+                sbo = frame.bands[b].subbands[sb]
+                if sb == main_subband:
+                    sbo.volume = 8
                 else:
-                    print "WARN: band", band, "main tone volume less than subband, swap"
-                    sb[0], sb[1] = sb[1], sb[0]
+                    sbo.volume = decode.meta.get_local_volume(main_volume, sbo.volume)
 
-            main_volume = 15
-            main_volume_k = 1
+        #print frame
+        d = frame.encode()
+        #print d
 
-            for k in main_volumes:
-                main_volume_k = k
-                if sb[0]>k:
-                    break
-                main_volume -= 1
-
-            print "band", band, "sb[0]", sb[0], "main volume", main_volume
-
-            sub_volume = 7
-            for k in local_volumes:
-                if sb[1] > k * main_volume_k:
-                    break
-                else:
-                    sub_volume -= 1
-            print "subband", 1, "val", val, "volume", sub_volume
-
-            frame[band] = main_volume
-            offset = 52
-            frame[offset] = sub_volume << 1 # ignoring phase
-            frame[offset+1] = 0 # ignoring overlapping subband
-
-        for band in (4, 3):
-            sb = []
-            max_val = 0
-            max_sub = 0
-            for sband in range(4):
-                val = freqs_data.get(band*250 + sband*62.5,0)
-                val = abs(val)/k0 * freq_resp[band]
-                sb.append(val)
-                if val>max_val:
-                    max_val = val
-                    max_sub = sband
-
-            if sb[0] < max_val: # subband value more than main tone
-                print "band", band, "WARN: main tone volume less than subband, swap", sb
-                sb[0], sb[max_sub] = sb[max_sub], sb[0]
-
-            main_volume = 15
-            main_volume_k = 1
-
-            for k in main_volumes:
-                main_volume_k = k
-                if sb[0]>k:
-                    break
-                main_volume -= 1
-
-            print "band", band, "sb[0]", sb[0], "main volume", main_volume
-
-            frame[band] = main_volume
-            offset = 40 + (band-3) * 6
-
-            for sub in (1,2,3):
-
-                sub_volume = 7
-                for k in local_volumes:
-                    if sb[sub] > k * main_volume_k:
-                        break
-                    else:
-                        sub_volume -= 1
-                print "subband", sub, "val", sb[sub], "volume", sub_volume
-                frame[offset+[0,0,1,3][sub]] = sub_volume << 1 # ignoring phase
-
-        prev_band_main_left = 0
-
-        for band in (2, 1, 0):
-            sb = []
-            max_val = 0
-            max_sub = 0
-
-            subs = 4
-            if prev_band_main_left:
-                subs = 5
-                prev_band_main_left = 0
-
-            for sub in range(subs):
-                val = freqs_data.get(band*250 + sub*62.5,0)
-                val = abs(val)/k0 * freq_resp[band]
-                sb.append(val)
-
-                if val>max_val:
-                    max_val = val
-                    max_sub = sub
-
-            main_volume = 15
-            main_volume_k = 1
-            for k in main_volumes:
-                main_volume_k = k
-                if max_val>k:
-                    break
-                main_volume -= 1
-
-            print "band", band, "max_val", max_val, "main volume", main_volume
-
-            main_offset = [0,1,3,5,7][max_sub]
-
-            frame[band] = main_volume
-            offset = 16 + band*8
-            frame[offset] = main_offset
-
-            for sub in range(subs):
-                val = sb[sub]
-                if sub == max_sub:
-                    sub_volume = 0
-                    print "subband", sub, "is main, skip"
-                elif sub == 0:
-                    print "subband", sub, "is base but not main, skip to next band"
-                    prev_band_main_left = 1
-                    sub_volume = 0
-                else:
-                    sub_volume = 7
-                    for k in local_volumes:
-                        if val > k * main_volume_k:
-                            break
-                        else:
-                            sub_volume -= 1
-                    print "subband", sub, "val", val, "volume", sub_volume
-                
-                sub_offset = [1,1,3,5,7][sub] # first 1 - for not overwriting main_offset
-
-                frame[offset+sub_offset] = sub_volume << 1
-
-        print frame
-
-        fdata = ""
-        for i in range(32):
-            val = chr(frame[i*2]|(frame[i*2+1]<<4))
-            fdata += val
-        out.write(fdata)
+        out.write(frame.pack(d))
 
         frame_num += 1
+        #if frame_num > 100:
+        #    break
     out.write("\xFF\xFF\x00\x00")
     print "super_max:", super_max
 
